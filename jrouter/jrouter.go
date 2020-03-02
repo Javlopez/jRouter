5b4652037b2a61377e639704c93c245f819a96f8
Javier Lopez Lopez <sjavierlopez@gmail.com>
2020-03-01 23:57:02 -0600

    Add support for multple methods in the same handler call

--- jrouter/jrouter.go
+++ jrouter/jrouter.go
@@ -1,42 +1,98 @@
 package jrouter
 
 import (
+	"errors"
 	"net/http"
+	"strings"
 )
+
+var AllowedMethods = []string{
+	http.MethodGet,
+	http.MethodPost,
+	http.MethodPut,
+	http.MethodDelete,
+}
+
+var MethodIsNotAllowedError = errors.New("The method is not allowed")
 
 //Handler is a type able to manage custom handler keeping http.ResponseWriter, *http.Request
 type Handler func(http.ResponseWriter, *http.Request)
 
 //JRouter struct
 type JRouter struct {
-	Routes []Route
+	Routes map[string]Route
 }
 
 //Route struct
 type Route struct {
 	Pattern string //*regexp.Regexp
 	Handler Handler
-	Methods string
+	Methods []string
 }
 
 //New function
 func New() *JRouter {
-	return &JRouter{}
+	return &JRouter{
+		Routes: make(map[string]Route),
+	}
 }
 
 //Handle method is to create routes from handlers
-func (jr *JRouter) Handle(pattern string, handler Handler, methods string) {
+func (jr *JRouter) Handle(pattern string, handler Handler, methods string) error {
 	//re := regexp.MustCompile(pattern)
-	route := Route{Pattern: pattern, Handler: handler, Methods: methods}
-	jr.Routes = append(jr.Routes, route)
+	methodsParsed, err := buildMethods(methods)
+	if err != nil {
+		return err
+	}
+
+	//fmt.Printf("[Routes Map: %#v]\n", jr.Routes[pattern])
+	if _, ok := jr.Routes[pattern]; ok {
+		currentMethods := jr.Routes[pattern].Methods
+		for _, mp := range methodsParsed {
+			methodFound := false
+			for _, m := range currentMethods {
+				if m == mp {
+					methodFound = true
+				}
+			}
+			if !methodFound {
+				methodsParsed = append(currentMethods, mp)
+			}
+		}
+	}
+
+	route := Route{Pattern: pattern, Handler: handler, Methods: methodsParsed}
+	jr.Routes[pattern] = route
+	return nil
 }
 
-/*
+//MethodIsAllowed validates is the method used by the request is allowed or not
+func methodIsAllowed(method string) bool {
+	for _, methodAllowed := range AllowedMethods {
+		if methodAllowed == method {
+			return true
+		}
+	}
+	return false
+}
 
+func buildMethods(methods string) ([]string, error) {
+	var methodsParsed []string
+	if !strings.Contains(methods, ",") {
+		if !methodIsAllowed(methods) {
+			return methodsParsed, MethodIsNotAllowedError
+		}
+		methodsParsed = append(methodsParsed, methods)
+		return methodsParsed, nil
+	}
 
+	methodsList := strings.Split(methods, ",")
+	for _, method := range methodsList {
+		if !methodIsAllowed(method) {
+			return methodsParsed, MethodIsNotAllowedError
+		}
+		methodsParsed = append(methodsParsed, method)
+	}
 
-jr := jrouter.New()
-r.Handle("endpoint", handler, "POST, GET")
-r.Handle("endpoint", handler, "POST, GET")
-r.Serve(8080)
-*/
+	return methodsParsed, nil
+}
--- jrouter/jrouter_test.go
+++ jrouter/jrouter_test.go
@@ -7,11 +7,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func HandlerSimpleSample(http.ResponseWriter, *http.Request) {
+func HandlerSimpleSample(http.ResponseWriter, *http.Request) {}
 
-}
+func TestGeneralRouter(t *testing.T) {
 
-func TestNewInstanceOfRouter(t *testing.T) {
 	t.Run("Should be able to run new Router instance using New function", func(t *testing.T) {
 		jr := New()
 		assert.IsType(t, &JRouter{}, jr)
@@ -22,5 +21,30 @@ func TestNewInstanceOfRouter(t *testing.T) {
 		jr.Handle("/some-end-point", HandlerSimpleSample, "POST")
 		assert.IsType(t, &JRouter{}, jr)
 		assert.Equal(t, len(jr.Routes), 1)
+	})
+	t.Run("Should reject the boostrap if the method is not allowd", func(t *testing.T) {
+		jr := New()
+		err := jr.Handle("/some-end-point", HandlerSimpleSample, "InvalidMethod")
+		assert.Equal(t, len(jr.Routes), 0)
+		assert.EqualError(t, err, "The method is not allowed")
+	})
+
+	t.Run("Should be able to group endpoint with different methods", func(t *testing.T) {
+		jr := New()
+		jr.Handle("/some-end-point", HandlerSimpleSample, "POST")
+		jr.Handle("/some-end-point", HandlerSimpleSample, "GET")
+		jr.Handle("/some-end-point", HandlerSimpleSample, "PUT")
+
+		assert.IsType(t, &JRouter{}, jr)
+		assert.Equal(t, len(jr.Routes), 1)
+	})
+
+	t.Run("Should be able to support different methods", func(t *testing.T) {
+		jr := New()
+		jr.Handle("/some-end-point", HandlerSimpleSample, "POST,GET")
+
+		assert.IsType(t, &JRouter{}, jr)
+		assert.Equal(t, 1, len(jr.Routes))
+		assert.Equal(t, len(jr.Routes["/some-end-point"].Methods), 2)
 	})
 }
